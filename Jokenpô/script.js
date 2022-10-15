e7ca454e9a11ae1a9170451e578fa9475558d477
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2022-10-15 02:02:50 -0300

    Jokenpô

---
+++ Jokenpô/script.js
@@ -0,0 +1,45 @@
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+
+let Inicio = prompt("Bem-vindo, digite 1 para Papel 📜 , 2 Para Pedra 🌑  ou 3 para Tesoura ✂️  Para jogar: ");
+var jogadas = (0)
+
+
+
+if (Inicio == 1){
+    console.log('Você usou Papel 📜 e');
+}
+if (Inicio == 2){
+    console.log('Você usou Pedra 🌑 e');
+}
+if (Inicio == 3){
+    console.log('Você usou Tesoura ✂️ e');
+}
+
+
+const bot = Math.floor(Math.random() * 3) + 1;
+
+if(bot == 1){
+    console.log('O bot usou Papel 📜');
+}else if (bot == 2){
+    console.log('O bot usou Pedra 🌑');
+}else if(bot == 3){
+    console.log('O bot usou Tesoura ✂️');
+}
+
+    if (bot == Inicio){
+        console.log(`Deu empate! ➖`)
+    } else if(bot == 2 && Inicio == 1){
+        console.log('Parabens você ganhou do bot! ✅ 🏆 Digite "4" para ver suas jogadas')
+    } else if (bot == 1 && Inicio == 2){
+        console.log('Você Perdeu do bot Tente novamente ❌')
+    } else if (bot == 1 && Inicio == 3){
+        console.log('Parabens você ganhou do bot! ✅ 🏆 Digite "4" para ver suas jogadas')
+    } else if (bot == 3 && Inicio == 1){
+        console.log('Você Perdeu do bot Tente novamente ❌')
+    } else if (bot == 2 && Inicio == 3){
+        console.log('Você Perdeu do bot Tente novamente ❌')
+    } else if (bot == 3 && Inicio == 2){
+        console.log(`Parabens você ganhou!✅ 🏆. Voce ganhou ${++jogadas} vezes. Aperte 4 para ir denovo`)
+    } 
