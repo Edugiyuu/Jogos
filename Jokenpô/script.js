978c14ecc132dc783b0f846729d24dae667eac55
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2022-10-19 17:58:18 -0300

    Update script.js

--- Jokenpô/script.js
+++ Jokenpô/script.js
@@ -1,51 +1,53 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-
-let Inicio = prompt("Bem-vindo, digite 1 para Papel 📜 , 2 Para Pedra 🌑  ou 3 para Tesoura ✂️  Para jogar: ");
-var jogadas = (0)
-
-
-
-if (Inicio == 1){
-    console.log('Você usou Papel 📜 e');
+var continuar = "1";
+function EscolhaDoplayer(numero) {
+    jokenpo = {
+    1:'Você usou Papel 📜',
+    2:'Você usou Pedra 🌑',
+    3:'Você usou Tesoura ✂️'
 }
-if (Inicio == 2){
-    console.log('Você usou Pedra 🌑 e');
-}
-if (Inicio == 3){
-    console.log('Você usou Tesoura ✂️ e');
+console.log(jokenpo[numero]);
 }
 
-
-const bot = Math.floor(Math.random() * 3) + 1;
-
-if(bot == 1){
-    console.log('O bot usou Papel 📜');
-}else if (bot == 2){
-    console.log('O bot usou Pedra 🌑');
-}else if(bot == 3){
-    console.log('O bot usou Tesoura ✂️');
+function EscolhaDoRobo(numero) {
+    jokenpo = {
+    1:'O Robo usou Papel 📜',
+    2:'O Robo usou Pedra 🌑',
+    3:'O Robo usou Tesoura ✂️'
+}
+console.log(jokenpo[numero]);
 }
 
-    if (bot == Inicio){
-        console.log(`Deu empate! ➖`)
-    } else if(bot == 2 && Inicio == 1){
-        console.log(`Parabens você ganhou do bot! ✅ 🏆. Voce ganhou ${++jogadas} vezes`)
-    } else if (bot == 1 && Inicio == 2){
-        console.log('Você Perdeu do bot Tente novamente ❌')
-    } else if (bot == 1 && Inicio == 3){
-        console.log(`Parabens você ganhou do bot! ✅ 🏆. Voce ganhou ${++jogadas} vezes`)
-    } else if (bot == 3 && Inicio == 1){
-        console.log('Você Perdeu do bot Tente novamente ❌')
-    } else if (bot == 2 && Inicio == 3){
-        console.log('Você Perdeu do bot Tente novamente ❌')
-    } else if (bot == 3 && Inicio == 2){
-        console.log(`Parabens você ganhou!✅ 🏆. Voce ganhou ${++jogadas} vezes`)
-    } 
-    let voltar = prompt(`Quer jogar novamente? responda "Sim" ou "Não"`);
+console.log('Bem vindo')
 
-    if(voltar == 'Sim' || `sim`){
-        
-    }else if(voltar == `Não` || `não`) {
-        
-    }
+while (continuar === "1") {
+  let Escolha = prompt(
+    `Digite 1 para Papel 📜 , 2 Para Pedra 🌑  ou 3 para Tesoura ✂️  Para jogar: ` 
+  );
+
+  (EscolhaDoplayer(Escolha));
+  
+  const Robo = Math.floor(Math.random() * 3) + 1;
+  (EscolhaDoRobo(Robo))
+
+  if (Robo == Escolha) {
+    console.log(`Deu empate! ➖`);
+  } else if (Robo == 2 && Escolha == 1) {
+    console.log(
+      `Parabens você ganhou do Robo! ✅ 🏆. Voce ganhou  vezes`
+    );
+  } else if (Robo == 1 && Escolha == 2) {
+    console.log("Você Perdeu do Robo Tente novamente ❌");
+  }
+  continuar = prompt(
+    `Quer jogar novamente? responda "1" para Sim ou "2" Para Não `
+  );
+}
+// Evitar a repetição da menssagem de vitoria
+// Evitar a repetição da menssagem de derrota
+// Criar Tratativa para a palavra vezes para quando for no plural e vez quando for singular
+// Alterar o nome da variavel Inicio, jogadas, bot. Feito
+// Criar Validação do que o usuario digitar
+// Utilizar objeto para substituição para os if
+// Criar função que mostra quem ganhou 'Dentro da função tera uma regra de como funciona o jogo essa função retornara quem ganhou'
