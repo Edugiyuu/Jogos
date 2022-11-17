7ceebbb92c2704785c4d717a9fff47d9c8ab6838
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2022-11-16 22:18:18 -0300

    Jokenpo-de-navegador

---
+++ Jokenpo-Navegador/script.js
@@ -0,0 +1,11 @@
+const buttonpvp = document.getElementById("pvpButton")
+const buttonpvb = document.getElementById("pvbButton")
+
+function pvpDisappear() {
+    buttonpvp.style.display = "none";
+    buttonpvb.style.display = "none";
+}
+function pvbDisappear() {
+    buttonpvb.style.display = "none";
+    buttonpvp.style.display = "none";
+}
