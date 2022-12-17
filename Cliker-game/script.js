b737c547ea63e93217dd0ffc5fbbb9bbf07914d9
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2022-12-17 14:57:19 -0300

    Update

--- Cliker-game/script.js
+++ Cliker-game/script.js
@@ -3,11 +3,28 @@ const DeterminacaoPng = document.querySelector('#DeterminacaoPng')
 var valor = 0
 
 function CadaClick() {
-
     NumerosDoContador.innerHTML = ++valor
 }
-
-
 DeterminacaoPng.addEventListener('click',CadaClick)
+DeterminacaoPng.addEventListener('click',AudioSelect)
+//----------------------------------------------------
+function AudioSelect() {
+    const AudioSelect = document.querySelector('#AudioSelect')
+    AudioSelect.play()
+}
 
-
+//----------------------------------------------------
+const PreçoPaciencia = document.querySelector('#PreçoPaciencia')
+const Pacience = document.querySelector('#Pacience')
+function CheckAndBuyPacience() {
+    if (valor > 30) {
+        alert('Paciencia Obtida');
+        NumerosDoContador.innerHTML = valor -=30
+        PreçoPaciencia.style.color = 'green'
+        PreçoPaciencia.innerHTML = 'Paciencia Obtida'
+    }else{
+        alert('Sem Determinação Suficiente');
+    }
+}
+Pacience.addEventListener('click', AudioSelect)
+Pacience.addEventListener('click',CheckAndBuyPacience)
