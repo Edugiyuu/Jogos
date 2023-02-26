987c071f99dcd6c6fa034ee2e4bd5b7422f30643
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2023-02-26 17:40:17 -0300

    Update script.js

--- Prova/script.js
+++ Prova/script.js
@@ -1,51 +1,43 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
 
-
 var perguntas = [
-  {
-    pergunta: "Em que ano o nintendo switch foi lançado?",
-    opcoes: "a) 2014, b) 2019, c) 2017",
-    resposta: "c"
-  },
-  {
-    pergunta: "Em que ano o xbox 360 foi lançado?",
-    opcoes: "a) 2013, b) 2005, c) 2010",
-    resposta: "b"
-  },
-  {
-    pergunta: "Em que ano o playstation 4 foi lançado?",
-    opcoes: "a) 2013, b) 2011, c) 2009",
-    resposta: "a"
-  },
+  "Em que ano o nintendo switch foi lançado?",  "Em que ano o xbox 360 foi lançado?",  "Em que ano o playstation 4 foi lançado?"
 ];
+var opcoes = [
+  "a) 2014, b) 2019, c) 2017",
+  "a) 2013, b) 2005, c) 2010",
+  "a) 2013, b) 2011, c) 2009"
+];
+
+var respostas = ["c", "b", "a"];
 
 
 var notaFinal = 0
 
 function perguntasErespostas() {
   for (let i = 0; i < perguntas.length; i++) {
-    console.log(perguntas[i].pergunta);
-    console.log(perguntas[i].opcoes);
-    var SuaResposta = prompt('Sua resposta: ')
+    console.log(perguntas[i]);
+    console.log(opcoes[i]);
+    var suaResposta = prompt('Sua resposta: ')
 
-    if (SuaResposta == perguntas[i].resposta) {
+    if (suaResposta == respostas[i]) {
       notaFinal += 1
     }
   }
 }
 
 function IniciarProva() {
-  let Iniciar = prompt(
+  let iniciar = prompt(
     "Deseja Iniciar a prova?(S/N):"
   );
 
-  if (Iniciar === 's') {
+  if (iniciar.toUpperCase() === 'S') {
     perguntasErespostas()
     console.log(`Sua nota foi ${notaFinal} de ${perguntas.length}`);
     
   }else{
-    Iniciar = prompt('')
+    iniciar = prompt('')
   }
 }
 
