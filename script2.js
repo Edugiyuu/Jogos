d4b78222efb48204ccc392b3bfcde5b48b098851
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2023-04-16 19:56:51 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -73,21 +73,3 @@ if (resposta == 's') {
     console.log('Tempo limite');
 } */
 
-/* const timeOut = 10000
-const tempoLimite = () => console.log('Tempo limite');
-
-let interval = setInterval(tempoLimite, timeOut);
-
-const IniciarTime = prompt('Deseja Iniciar o tempo?(S/N)')
-
-if (IniciarTime === 'S') {
-    setTimeout( () => clearInterval(interval),10000)
-}
-
- const pararAntes = prompt('digite algo')
-
-if (pararAntes == 'S') {
-    clearTimeout(interval)
-} */
-
-
